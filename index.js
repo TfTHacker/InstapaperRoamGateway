df47e9242e28d2b4952f84fbe1ac2d0477487caa
roamhacker <roamhacker@gmail.com>
2021-01-08 08:02:18 +0000

    initial working code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const instapaper = require('./instapaper')
 const express = require('express');
 const app = express();
-const url = require("url")
+const url = require("url");
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -10,9 +11,49 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test', function(request,response){
-	response.send({hi:'test'})
+	response.send({test: 'Server running' })
 })
 
+app.get('/gettoken', async function(request,response){
+	const client = new instapaper(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
+	client.setUserCredentials(process.env.USERNAME, process.env.PASSWORD)
+	const token = await client.getOAuthTokenAndSecret();
+	response.send(token);
+})
+
+// Following functions expect
+// username and password as url's on the GET 
+
+const configClient = async (params)=>{
+	const client = new instapaper(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
+	client.setUserCredentials(params.username, params.password);
+	return client;
+}
+
+app.get('/unread', async function(request,response){
+	const params = url.parse(request.url,true).query;
+	const client = await configClient(params);
+	const rawResults = await client._request('/api/1/bookmarks/list', {folder_id: 'unread'} );
+	let results = rawResults.data.map(r => r);
+	response.send(results);
+})
+
+app.get('/highlights', async function(request,response){
+	var params = url.parse(request.url,true).query;
+	if(params.id) {
+		const client = await configClient(params);
+		try{
+			const results = (await client._request('/api/1.1/bookmarks/' + params.id + '/highlights'));
+			let highlights = results.data.map(h => h);
+			response.send(highlights);
+		} catch(e) {
+			response.send('invalid bookmark id');
+			console.log(e)
+		}
+	} else {
+		response.send('missing parameter id');
+	}
+})
 
 
 app.listen(3000, () => {
